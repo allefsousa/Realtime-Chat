1a49a0b2bddf37ab89f52f7594cbf4368b1b5df9
Allef Sousa <allefsousa_1@hotmail.com>
2020-04-21 22:22:44 -0300

    Criando  o ListView para exibir os dados do firebase

--- lib/pages/chat_screen.dart
+++ lib/pages/chat_screen.dart
@@ -11,22 +11,21 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  void _sendMessage({String text, File imgFile}) async {
+    Map<String, dynamic> messageData = {};
 
-  void _sendMessage({String text, File imgFile}) async{
-
-    Map<String,dynamic> messageData = {};
-
-    if(imgFile != null){
-      StorageUploadTask task = FirebaseStorage.instance.ref().child(
-        DateTime.now().millisecondsSinceEpoch.toString()
-      ).putFile(imgFile);
+    if (imgFile != null) {
+      StorageUploadTask task = FirebaseStorage.instance
+          .ref()
+          .child(DateTime.now().millisecondsSinceEpoch.toString())
+          .putFile(imgFile);
 
       StorageTaskSnapshot taskSnapshot = await task.onComplete;
       String url = await taskSnapshot.ref.getDownloadURL();
       messageData["imageUrl"] = url;
-      print("image url"+url);
+      print("image url" + url);
     }
-    if(text != null) messageData["text"] = text;
+    if (text != null) messageData["text"] = text;
 
     Firestore.instance.collection("messages").add(messageData);
   }
@@ -36,8 +35,38 @@ class _ChatScreenState extends State<ChatScreen> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Olá'),
+        elevation: 0.0,
       ),
-      body: TextCompose( _sendMessage),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+              child: StreamBuilder<QuerySnapshot>(
+                stream: Firestore.instance.collection("messages").snapshots(),
+                builder: (context, snapshot) {
+              switch (snapshot.connectionState) {
+                case ConnectionState.none:
+                case ConnectionState.waiting:
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                default:
+                  List<DocumentSnapshot> documents = snapshot.data.documents.reversed.toList();
+
+                  return ListView.builder(
+                    itemCount: documents.length,
+                    reverse: true,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        title: Text(documents[index].data['text']),
+                      );
+                    },
+                  );
+              }
+            },
+          )),
+          TextCompose(_sendMessage),
+        ],
+      ),
     );
   }
 }
